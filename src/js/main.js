573cc721781bbac4cd7b114d158902ffdfe847b1
mattmalicki <mateusz.malicki96@gmail.com>
2023-08-18 16:26:47 +0200

    fun

--- src/js/main.js
+++ src/js/main.js
@@ -6,39 +6,37 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadButton = document.querySelector('.load-more');
-let page = 1;
-let totalHits = 0;
+const iconsPath = {
+  likes: './img/icons.svg#icon-likes',
+  views: './img/icons.svg#icon-eye',
+  comments: './img/icons.svg#icon-comment',
+  downloads: './img/icons.svg#icon-download',
+};
+const obj = {
+  page: 1,
+  totalHits: 0,
+  formValue: '',
+};
+const lightbox = new simpleLightbox('.gallery img', {
+  sourceAttr: 'data-big-img',
+});
 
 formEl.addEventListener('submit', event => {
   loadButton.classList.add('is-hidden');
+  obj.formValue = formEl.firstElementChild.value;
   event.preventDefault();
   page = 1;
-  submitForm();
-  loadButton.classList.remove('is-hidden');
+  submitForm(obj.formValue, obj.page);
 });
 
 loadButton.addEventListener('click', () => {
-  fetchImages(formEl.firstElementChild.value, page)
-    .then(function (response) {
-      return response;
-    })
-    .then(function (images) {
-      createGallery(images.hits);
-      createSimpleLightbox();
-      page++;
-      if (totalHits === galleryEl.childNodes.length) {
-        failMessage(true);
-        loadButton.classList.add('is-hidden');
-      }
-    })
-    .catch(function (e) {
-      console.log(e);
-    });
+  loadMore(obj.formValue, obj.page);
 });
+
+// generating images
 
 function createGallery(images) {
   if (images.length === 0) {
-    failMessage(false);
     return;
   }
   const cardsArray = createCards(images);
@@ -69,29 +67,58 @@ function createCardsInfo(image) {
     const infoEl = document.createElement('p');
     infoEl.classList.add('info-item');
     const textInfo = image[`${info}`];
-    infoEl.insertAdjacentHTML('beforeend', `<b>${info}</b>${textInfo}`);
+    const icon = iconsPath[`${info}`];
+    infoEl.insertAdjacentHTML(
+      'beforeend',
+      `<svg width="15" height="15" class="icon"><use href="${icon}"></use></svg><p>${textInfo}</p>`
+    );
     infoDiv.append(infoEl);
   });
   return infoDiv;
 }
 
-function submitForm() {
+// button functions
+
+function submitForm(value, page) {
   galleryEl.innerHTML = '';
-  fetchImages(formEl.firstElementChild.value, page)
+  fetchImages(value, page)
     .then(function (response) {
-      totalMessage(response.totalHits);
-      totalHits = response.totalHits;
+      response.totalHits === 0
+        ? failMessage(false)
+        : totalMessage(response.totalHits);
+      obj.totalHits = response.totalHits;
       return response;
     })
     .then(function (images) {
       createGallery(images.hits);
-      page++;
-      createSimpleLightbox();
+      obj.page++;
+      lightbox.refresh();
     })
     .catch(function (e) {
       console.log(e);
     });
 }
+
+function loadMore(value, page) {
+  fetchImages(value, page)
+    .then(function (response) {
+      return response;
+    })
+    .then(function (images) {
+      createGallery(images.hits);
+      lightbox.refresh();
+      page++;
+      if (obj.totalHits === galleryEl.childNodes.length) {
+        failMessage(true);
+        loadButton.classList.add('is-hidden');
+      }
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+}
+
+// Notiflix messages
 
 function failMessage(loadAll) {
   if (loadAll) {
@@ -106,11 +133,6 @@ function failMessage(loadAll) {
 }
 
 function totalMessage(totalHits) {
+  loadButton.classList.remove('is-hidden');
   new Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-}
-
-function createSimpleLightbox() {
-  const lightbox = new simpleLightbox('.gallery img', {
-    sourceAttr: 'data-big-img',
-  });
 }
